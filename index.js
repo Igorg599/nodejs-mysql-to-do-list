f3f80db47ec07baf7d1b24a13a018e4d19a33436
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2021-07-22 19:50:29 +0500

    customization

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const express = require('express')
-const path = require('path')
-const todoRoutes = require('./routes/todo')
+const express = require("express")
+const path = require("path")
+const { graphqlHTTP } = require("express-graphql")
+const sequelize = require("./utils/database")
+const schema = require("./graohql/schema")
+const resolver = require("./graohql/resolver")
 const app = express()
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, "public")))
+app.use(express.json())
 
-app.use('/api/todo', todoRoutes)
+app.use(
+  graphqlHTTP({
+    schema: schema,
+    rootValue: resolver,
+  })
+)
 
 app.use((req, res, next) => {
-  res.sendFile('/index.html')
+  res.sendFile("/index.html")
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+  try {
+    await sequelize.sync()
+    app.listen(PORT)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
--- routes/todo.js
+++ routes/todo.js
@@ -1,25 +1,68 @@
 const {Router} = require('express')
+const Todo = require('../models/todo')
 const router = Router()
 
-//Получение списка задач
-router.get('/', (req, res) => {
- res.json({a: 1})
+// Получение списка задач
+router.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.findAll()
+    res.status(200).json(todos)
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
-//Создание новой задачи
-router.post('/', (req, res) => {
-
+// Создание новой задачи
+router.post('/', async (req, res) => {
+  try {
+    const todo = await Todo.create({
+      title: req.body.title,
+      done: false
+    })
+    res.status(201).json({todo})
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
-//Изменение задач
-router.put('/:id', (req, res) => {
-
+// Изменение задачи
+router.put('/:id', async (req, res) => {
+  try {
+    const todo = await Todo.findByPk(+req.params.id)
+    todo.done = req.body.done
+    await todo.save()
+    res.status(200).json({todo})
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
-
-//Удаление задач
-router.delete('/:id', (req, res) => {
-
+// Удаление задачи
+router.delete('/:id', async (req, res) => {
+  try {
+    const todos = await Todo.findAll({
+      where: {
+        id: +req.params.id
+      }
+    })
+    const todo = todos[0]
+    await todo.destroy()
+    res.status(204).json({})
+  } catch (e) {
+    console.log(e)
+    res.status(500).json({
+      message: 'Server error'
+    })
+  }
 })
 
 module.exports = router
